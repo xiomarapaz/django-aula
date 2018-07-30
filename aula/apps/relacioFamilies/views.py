d5ca09d023be541b0c8d28eb8e3c35cd162e098f
dani herrera <ctrl.alt.d@gmail.com>
2018-07-30 12:26:55 +0200

    qr amb json dades basiques

--- aula/apps/relacioFamilies/views.py
+++ aula/apps/relacioFamilies/views.py
@@ -195,9 +195,13 @@ def qrTokens( request , pk=None ):
         qr_token.calcula_clau_i_localitzador()
         qr_token.alumne_referenciat = alumne
         qr_token.save()
-
-        qr_text = r"{token}".format( token=qr_token.clau )
-
+        qr_dict = { "key": qr_token.clau,
+                    "id": alumne.id,
+                    "name": alumne.nom,
+                    "api_end_point": settings.URL_DJANGO_AULA,
+                    "organization": settings.NOM_CENTRE,
+                    }
+        qr_text = json.dumps(qr_dict)
 
         # munto imatge:
         nom_fitxer = r"/tmp/barcode-{0}-{1}-{2}-{3}.png".format( time.time(), 
--- aula/mblapp/models.py
+++ aula/mblapp/models.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# El token qr (QRPortal) és a Usuaris.
